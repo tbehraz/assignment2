6affaaf1b51ba8bcd8354ed48c84d34e0a670a4f
tbehraz <109410879+tbehraz@users.noreply.github.com>
2022-11-25 12:58:34 +0330

    Update fileclient.sh

--- fileclient.sh
+++ fileclient.sh
@@ -1,2 +1,5 @@
 
 docker volume create --name clientvol --opt type=none --opt device=/clientdata --opt o=bind
+
+docker volume create clientvol
+docker run -d --name Client --mount source=clientvol,target=/clientdata busybox:latest
