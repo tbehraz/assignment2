0cb318625dc1e544397fa66a414a0a81eb806130
tbehraz <109410879+tbehraz@users.noreply.github.com>
2022-11-27 18:07:37 +0330

    Update fileserver.sh

--- fileserver.sh
+++ fileserver.sh
@@ -1,4 +1,7 @@
-docker volume create --name servervol --opt type=none --opt device=/serverdata --opt o=bind
+docker volume create servervol
+
+docker run -d --name server --mount source=servervol,target=/serverdata nginx:alpine
 
 docker run bash:latest
 bash -c "base64 /dev/urandom | head -c 2000 > file.txt"
+
